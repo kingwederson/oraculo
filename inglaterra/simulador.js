378147cdd98e6d711d0ffa358cbc6f4f59c91943
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2024-12-19 19:11:26 -0300

    Fixing bugs

--- inglaterra/simulador.js
+++ inglaterra/simulador.js
@@ -2,8 +2,8 @@ function simular(){
     for(let l=1; l<=20;l++){
         for(let m=1; m<=20;m++){
             if(l!=m && (tabelaRR[l][m][0] == null && tabelaRR[l][m][1] == null)){
-                casa = (equipe[l][2].a/equipe[m][2].d)/2
-                fora = (equipe[m][2].a/equipe[l][2].d)/2
+                casa = (equipe[l][2].a+equipe[m][2].d)/2
+                fora = (equipe[m][2].a+equipe[l][2].d)/2
                 tabelaRR[l][m][0] = golmarcado(casa)
                 tabelaRR[l][m][1] = golmarcado(fora)
             }
