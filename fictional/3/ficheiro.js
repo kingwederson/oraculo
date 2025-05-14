461c7a7ea6ec09f9d86b79ca00800c5a5da0aad0
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2025-05-14 15:17:48 -0300

    Add files via upload

---
+++ fictional/3/ficheiro.js
@@ -0,0 +1,93 @@
+var equipe = [
+    [`Patel League`,`../imagem/pat1.png`,,0],
+    //[`Aegis`,`../imagem/aegis.png`,{a:28/38,d:72/38, rat:1800},21],
+    //[`Albaduz`,`../imagem/albaduz.png`,{a:52/38,d:48/38, rat:1800},15],
+    //[`Anconia`,`../imagem/anconia.png`,{a:30/38,d:70/38, rat:1800},22],
+    //[`Astro`,`../imagem/astro.png`,{a:70/38,d:30/38, rat:1800},1],
+    //[`Aurania`,`../imagem/aurania.png`,{a:54/38,d:46/38, rat:1800},14],
+    //[`Badenia`,`../imagem/badenia.png`,{a:66/38,d:34/38, rat:1800},2],
+    [`Beez`,`../imagem/beez.png`,{a:18/38,d:60/38, rat:1800},45],
+    [`Blackyorn`,`../imagem/blackyorn.png`,{a:18/38,d:60/38, rat:1800},46],
+    //[`Bolderpool`,`../imagem/bolderpool.png`,{a:29/38,d:61/38, rat:1800},34],
+    //[`Dalgalar`,`../imagem/dalgalar.png`,{a:34/38,d:66/38, rat:1800},20],
+    //[`Deneper`,`../imagem/deneper.png`,{a:40/38,d:60/38, rat:1800},16],
+    //[`Diamond`,`../imagem/diamond.png`,{a:18/38,d:82/38, rat:1800},27],
+    //[`Dizeler`,`../imagem/dizeler.png`,{a:44/38,d:56/38, rat:1800},17],
+    //[`Dover`,`../imagem/dover.png`,{a:36/38,d:64/38, rat:1800},35],
+    //[`Drax`,`../imagem/drax.png`,{a:46/38,d:54/38, rat:1800},9],
+    //[`Excester`,`../imagem/excester.png`,{a:20/38,d:65/38, rat:1800},39],
+    //[`Finitz`,`../imagem/finitz.png`,{a:20/38,d:80/38, rat:1800},33],
+    //[`Foxhill`,`../imagem/foxhill.png`,{a:20/38,d:80/38, rat:1800},29],
+    [`Fredalt`,`../imagem/fredalt.png`,{a:18/38,d:60/38, rat:1800},48],
+    //[`Frostheim`,`../imagem/frostheim.png`,{a:18/38,d:80/38, rat:1800},42],
+    [`Galt`,`../imagem/galt.png`,{a:26/38,d:30/38, rat:1800},58],
+    [`Gantov`,`../imagem/gantov.png`,{a:18/38,d:60/38, rat:1800},30],
+    //[`Glader`,`../imagem/glader.png`,{a:56/38,d:44/38, rat:1800},8],
+    //[`Golem`,`../imagem/golem.png`,{a:20/38,d:80/38, rat:1800},26],
+    [`Guardians`,`../imagem/guardians.png`,{a:36/38,d:64/38, rat:1800},19],
+    //[`Kelapan`,`../imagem/kelapan.png`,{a:38/38,d:62/38, rat:1800},18],
+    //[`Lanz`,`../imagem/lanz.png`,{a:58/38,d:42/38, rat:1800},12],
+    //[`Monarchs`,`../imagem/monarchs.png`,{a:42/38,d:58/38, rat:1800},13],
+    //[`Mosker`,`../imagem/mosker.png`,{a:18/38,d:80/38, rat:1800},47],
+    [`Naurer`,`../imagem/naurer.png`,{a:0/38,d:100/38, rat:1800},49],
+    [`Neron`,`../imagem/neron.png`,{a:0/38,d:100/38, rat:1800},54],
+    //[`Netz`,`../imagem/netz.png`,{a:18/38,d:80/38, rat:1800},43],
+    //[`Norpago`,`../imagem/norpago.png`,{a:48/38,d:52/38, rat:1800},11],
+    [`Odon`,`../imagem/odon.png`,{a:0/38,d:74/38, rat:1800},55],
+    //[`Orion`,`../imagem/orion.png`,{a:26/38,d:74/38, rat:1800},24],
+    //[`Pavan`,`../imagem/pavan.png`,{a:32/38,d:58/38, rat:1800},36],
+    //[`Pegasus`,`../imagem/pegasus.png`,{a:62/38,d:38/38, rat:1800},7],
+    //[`Phalguna`,`../imagem/phalguna.png`,{a:60/38,d:40/38, rat:1800},6],
+    [`Pioner`,`../imagem/pioner.png`,{a:0/38,d:100/38, rat:1800},51],
+    //[`Pollux`,`../imagem/pollux.png`,{a:18/38,d:80/38, rat:1800},44],
+    [`Prandober`,`../imagem/pranduber.png`,{a:0/38,d:100/38, rat:1800},60],
+    [`Prius`,`../imagem/prius.png`,{a:0/38,d:80/38, rat:1800},56],
+    //[`Rochester`,`../imagem/rochester.png`,{a:18/38,d:82/38, rat:1800},38],
+    [`Rubor`,`../imagem/rubor.png`,{a:0/38,d:100/38, rat:1800},53],
+    [`Ronsdale`,`../imagem/ronsdale.png`,{a:18/38,d:82/38, rat:1800},41],
+    [`Saldrim`,`../imagem/saldrim.png`,{a:0/38,d:100/38, rat:1800},59],
+    [`Shamesh`,`../imagem/shamesh.png`,{a:18/38,d:62/38, rat:1800},31],
+    [`Skarbor`,`../imagem/skarbor.png`,{a:0/38,d:100/38, rat:1800},57],
+    [`Stones`,`../imagem/stones.png`,{a:22/38,d:68/38, rat:1800},23],
+    //[`Taggart`,`../imagem/taggart.png`,{a:32/38,d:68/38, rat:1800},28],
+    //[`Talmikan`,`../imagem/talmikan.png`,{a:58/38,d:42/38, rat:1800},5],
+    [`Talon`,`../imagem/talon.png`,{a:0/38,d:100/38, rat:1800},50],
+    //[`Taurus`,`../imagem/taurus.png`,{a:64/38,d:36/38, rat:1800},4],
+    //[`Tivan`,`../imagem/tivan.png`,{a:24/38,d:66/38, rat:1800},37],
+    [`Tugmax`,`../imagem/tugmax.png`,{a:0/38,d:100/38, rat:1800},52],
+    //[`Tusler`,`../imagem/tusler.png`,{a:68/38,d:32/38, rat:1800},3],
+    //[`Webbos`,`../imagem/webbos.png`,{a:50/38,d:50/38, rat:1800},10],
+    //[`Werdenos`,`../imagem/werdenos.png`,{a:30/38,d:70/38, rat:1800},40],
+    //[`Zahav`,`../imagem/zahav.png`,{a:24/38,d:76/38, rat:1800},25],
+    //[`Zelta`,`../imagem/zelta.png`,{a:24/38,d:76/38, rat:1800},32]
+]
+
+var tabelaRRinicial = [[equipe[0][0]]]
+
+function criarRR(){
+    for(let r = 1;r<equipe.length;r++){
+        tabelaRRinicial[0].push(equipe[r][0])
+    }
+    for(let r = 1;r<equipe.length;r++){
+        tabelaRRinicial.push([equipe[r][0]])
+    }
+    for(let r = 1;r<equipe.length;r++){
+        for(let s=1;s<equipe.length;s++){
+            tabelaRRinicial[r].push([,])
+        }
+    }
+}
+/*
+var tabelaRRinicial = [[equipe[0][0]]]
+for(let r = 1;r<=1+equipe.length;r++){
+    tabelaRRinicial[0].push(equipe[r][0])
+}
+for(let r = 1;r<=1+equipe.length;r++){
+    tabelaRRinicial.push(equipe[r][0])
+}
+for(let r = 1;r<=1+equipe.length;r++){
+    for(let s=1;s<=1+equipe.length;s++){
+        tabelaRRinicial[r].push([,])
+    }
+}
+*/
---
+++ fictional/pes/ficheiro.js
@@ -0,0 +1,102 @@
+var equipe = [
+    [`Patel League`,`../imagem/pat1.png`,,0],
+    [`Against All Odds`,`../imagem/pes/againstallodds.png`,{a:20/38,d:50/38},1],
+    [`Alashergokh`,`../imagem/pes/alashergokh.png`,{a:20/38,d:50/38},2],
+    [`Almchendolf`,`../imagem/pes/almchendolf.png`,{a:20/38,d:20/38},3],
+    [`Altiplano`,`../imagem/pes/altiplano.png`,{a:20/38,d:50/38},4],
+    [`Aruzutanque`,`../imagem/pes/aruzutanque.png`,{a:20/38,d:50/38},5],
+    [`Ayatohua`,`../imagem/pes/ayatohua.png`,{a:20/38,d:50/38},6],
+    [`Babilayna`,`../imagem/pes/babilayna.png`,{a:20/38,d:20/38},7],
+    [`Bharshapur`,`../imagem/pes/bharshapur.png`,{a:20/38,d:50/38},8],
+    [`Biswerhausen`,`../imagem/pes/biswerhausen.png`,{a:20/38,d:50/38},9],
+    [`Blookrows`,`../imagem/pes/blookrows.png`,{a:20/38,d:20/38},10],
+    [`Cantlesir`,`../imagem/pes/cantlesir.png`,{a:20/38,d:50/38},11],
+    [`Celuvaris`,`../imagem/pes/celuvaris.png`,{a:20/38,d:20/38},12],
+    [`Cerchizmajiu`,`../imagem/pes/cerchizmajiu.png`,{a:20/38,d:50/38},13],
+    [`Creussenburg`,`../imagem/pes/creussenburg.png`,{a:20/38,d:50/38},14],
+    [`Dhroffsmitz`,`../imagem/pes/dhroffsmitz.png`,{a:20/38,d:50/38},15],
+    [`EFB`,`../imagem/pes/EFB.png`,{a:20/38,d:50/38},16],
+    [`Ehrenhofstadt`,`../imagem/pes/ehrenhofstadt.png`,{a:20/38,d:20/38},17],
+    [`Evolution`,`../imagem/pes/evolution.png`,{a:20/38,d:50/38},18],
+    [`Fineseeberg`,`../imagem/pes/fineseeberg.png`,{a:20/38,d:20/38},19],
+    [`Ganzoraccio`,`../imagem/pes/ganzoraccio.png`,{a:20/38,d:50/38},21],
+    [`Gharnetova`,`../imagem/pes/gharnetova.png`,{a:20/38,d:20/38},22],
+    [`Hammer Jungs`,`../imagem/pes/hammerjungs.png`,{a:20/38,d:50/38},20],
+    [`Herismakhgia`,`../imagem/pes/herismakhgia.png`,{a:20/38,d:50/38},23],
+    [`Hjorwesland`,`../imagem/pes/hjorwesland.png`,{a:20/38,d:50/38},24],
+    [`Iolceanicu`,`../imagem/pes/iolceanicu.png`,{a:20/38,d:50/38},25],
+    [`Johachnaard`,`../imagem/pes/johachnaard.png`,{a:20/38,d:50/38},26],
+    [`Jorudberg`,`../imagem/pes/jorudberg.png`,{a:20/38,d:50/38},27],
+    [`Kholugavinsk`,`../imagem/pes/kholugavinsk.png`,{a:20/38,d:50/38},28],
+    [`Kirksburgh`,`../imagem/pes/kirksburgh.png`,{a:20/38,d:50/38},29],
+    [`Kochglehmann`,`../imagem/pes/kochglehmann.png`,{a:20/38,d:50/38},30],
+    [`Kriedbach`,`../imagem/pes/kriedbach.png`,{a:20/38,d:50/38},31],
+    [`Kyljmeeski`,`../imagem/pes/kyljmeeski.png`,{a:20/38,d:50/38},32],
+    [`Lengerblitz`,`../imagem/pes/lengerblitz.png`,{a:20/38,d:20/38},33],
+    [`Ljakovnica`,`../imagem/pes/ljakovnica.png`,{a:20/38,d:50/38},34],
+    [`Ljemorac`,`../imagem/pes/ljemorac.png`,{a:20/38,d:50/38},35],
+    [`Maarsfort`,`../imagem/pes/maarsfort.png`,{a:20/38,d:20/38},36],
+    [`Marchidegna`,`../imagem/pes/marchidegna.png`,{a:20/38,d:50/38},37],
+    [`Marguaparrena`,`../imagem/pes/marguaparrena.png`,{a:20/38,d:20/38},38],
+    [`Meirkugaurt`,`../imagem/pes/meirkugaurt.png`,{a:20/38,d:50/38},39],
+    [`Meuchawyz`,`../imagem/pes/meuchawyz.png`,{a:20/38,d:50/38},40],
+    [`Mrabspor`,`../imagem/pes/mrabspor.png`,{a:20/38,d:50/38},41],
+    [`Mythloxanthi`,`../imagem/pes/mythloxanthi.png`,{a:20/38,d:50/38},42],
+    [`Nakhqachev`,`../imagem/pes/nakhqachev.png`,{a:20/38,d:20/38},43],
+    [`Nelapoltsk`,`../imagem/pes/nelapoltsk.png`,{a:20/38,d:20/38},44],
+    [`Nitsaloskis`,`../imagem/pes/nitsaloskis.png`,{a:20/38,d:20/38},45],
+    [`Nortovka`,`../imagem/pes/nortovka.png`,{a:20/38,d:50/38},46],
+    [`Odersteich`,`../imagem/pes/odersteich.png`,{a:20/38,d:50/38},47],
+    [`Pabrowiestein`,`../imagem/pes/pabrowiestein.png`,{a:20/38,d:20/38},48],
+    [`Punoichongaux`,`../imagem/pes/punoichongaux.png`,{a:20/38,d:50/38},49],
+    [`Quarpelather`,`../imagem/pes/quarpelather.png`,{a:20/38,d:50/38},50],
+    [`Raltonvegua`,`../imagem/pes/raltonvegua.png`,{a:20/38,d:20/38},51],
+    [`Ryagzhilograd`,`../imagem/pes/ryagzhilograd.png`,{a:20/38,d:50/38},52],
+    [`Saintragler`,`../imagem/pes/saintragler.png`,{a:20/38,d:20/38},53],
+    [`Salsabie`,`../imagem/pes/salsabie.png`,{a:20/38,d:50/38},54],
+    [`Sarmtonburg`,`../imagem/pes/sarmtonburg.png`,{a:20/38,d:50/38},55],
+    [`Serignaluca`,`../imagem/pes/serignaluca.png`,{a:20/38,d:50/38},56],
+    [`Squanoer`,`../imagem/pes/squanoer.png`,{a:20/38,d:50/38},57],
+    [`Szekegyvar`,`../imagem/pes/szekegyvar.png`,{a:20/38,d:50/38},58],
+    [`Szelawce`,`../imagem/pes/szelawce.png`,{a:20/38,d:50/38},59],
+    [`Tedloghec`,`../imagem/pes/tedloghec.png`,{a:20/38,d:50/38},60],
+    [`Theeselvargen`,`../imagem/pes/theeselvargen.png`,{a:20/38,d:50/38},61],
+    [`Trunecan`,`../imagem/pes/trunecan.png`,{a:20/38,d:50/38},62],
+    [`Verwestadt`,`../imagem/pes/verwestadt.png`,{a:20/38,d:50/38},63],
+    [`Vlieergroom`,`../imagem/pes/vlieergroom.png`,{a:20/38,d:50/38},64],
+    [`Waryamosuk`,`../imagem/pes/waryamosuk.png`,{a:20/38,d:50/38},65],
+    [`Wondengine`,`../imagem/pes/wondengine.png`,{a:20/38,d:50/38},66],
+    [`Xakoulagos`,`../imagem/pes/xakoulagos.png`,{a:20/38,d:50/38},67],
+    [`Zagrepaqu`,`../imagem/pes/zagrepaqu.png`,{a:20/38,d:50/38},68],
+    //[``,`../imagem/pes/.png`,{a:20/38,d:50/38},69]
+]
+
+var tabelaRRinicial = [[equipe[0][0]]]
+
+function criarRR(){
+    for(let r = 1;r<equipe.length;r++){
+        tabelaRRinicial[0].push(equipe[r][0])
+    }
+    for(let r = 1;r<equipe.length;r++){
+        tabelaRRinicial.push([equipe[r][0]])
+    }
+    for(let r = 1;r<equipe.length;r++){
+        for(let s=1;s<equipe.length;s++){
+            tabelaRRinicial[r].push([,])
+        }
+    }
+}
+/*
+var tabelaRRinicial = [[equipe[0][0]]]
+for(let r = 1;r<=1+equipe.length;r++){
+    tabelaRRinicial[0].push(equipe[r][0])
+}
+for(let r = 1;r<=1+equipe.length;r++){
+    tabelaRRinicial.push(equipe[r][0])
+}
+for(let r = 1;r<=1+equipe.length;r++){
+    for(let s=1;s<=1+equipe.length;s++){
+        tabelaRRinicial[r].push([,])
+    }
+}
+*/
