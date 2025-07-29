d109dd85409f4fecf8f2f1d9281f608c13f683aa
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2025-07-29 12:02:12 -0300

    Add files via upload

---
+++ brasil/2023a/regras.js
@@ -0,0 +1,18 @@
+function compara(alfa, omega){
+    if(omega[3]==alfa[3]){
+        if(omega[4]==alfa[4]){
+            if(omega[7]==alfa[7]){
+                if(omega[8]==alfa[8]){
+                    //console.log('empatou')
+                    return Math.random()-0.5
+                }
+                return omega[8]-alfa[8]
+            }
+            return omega[7]-alfa[7]
+        }else{
+            return omega[4]-alfa[4]
+        }
+    }else{
+        return omega[3]-alfa[3]
+    }
+}
