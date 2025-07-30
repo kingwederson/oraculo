0a313e559ad7dc24cebb5f68654fc8bd007e7dc0
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2025-07-30 09:38:18 -0300

    Add files via upload

--- brasil/a/regras.js
+++ brasil/a/regras.js
@@ -3,8 +3,14 @@ function compara(alfa, omega){
         if(omega[4]==alfa[4]){
             if(omega[7]==alfa[7]){
                 if(omega[8]==alfa[8]){
-                    console.log('empatou')
-                    return Math.random()-0.5
+                    if(omega[24]==alfa[24]){
+                        if(omega[25]==alfa[25]){
+                            console.log(`${omega[3]} | ${omega[4]} ${omega[5]} ${omega[6]} | ${omega[7]} ${omega[8]} ${omega[9]} || ${omega[11]} ${omega[12]} ${omega[13]} | ${omega[14]} ${omega[15]} ${omega[16]} || ${omega[19]} ${omega[20]} ${omega[21]} | ${omega[22]} ${omega[23]} ${omega[24]} \n${alfa[3]} | ${alfa[4]} ${alfa[5]} ${alfa[6]} | ${alfa[7]} ${alfa[8]} ${alfa[9]} || ${alfa[11]} ${alfa[12]} ${alfa[13]} | ${alfa[14]} ${alfa[15]} ${alfa[16]} || ${alfa[19]} ${alfa[20]} ${alfa[21]} | ${alfa[22]} ${alfa[23]} ${alfa[24]}`)
+                            return Math.random()-0.5
+                        }
+                        return alfa[25]-omega[25]
+                    }
+                    return omega[24]-alfa[24]
                 }
                 return omega[8]-alfa[8]
             }
