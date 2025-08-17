9834cda33681f0a0b566c38270e9a42446b51126
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2025-08-17 14:24:46 -0300

    Add files via upload

---
+++ inglaterra/ficheiro.js
@@ -0,0 +1,45 @@
+var rodat = 1
+var rodan = 0
+var equipe = [
+    [`Premier League`,`http://oraculo.wederson.com/inglaterra/imagem/eng1.png`,,0],// 0
+    [`Arsenal`,`http://oraculo.wederson.com/inglaterra/imagem/arsenal.png`,
+        {a:ajuste(10,rodat,69/38,1),d:ajuste(10,rodat,34/38,0),rat:2308},1], // 1
+    [`Aston Villa`,`http://oraculo.wederson.com/inglaterra/imagem/astonvilla.png`,
+        {a:ajuste(10,rodat,58/38,0),d:ajuste(10,rodat,51/38,0),rat:2308},2],// 2
+    [`Bournemouth`,`http://oraculo.wederson.com/inglaterra/imagem/bournemouth.png`,
+        {a:ajuste(10,rodat,58/38,2),d:ajuste(10,rodat,46/38,4),rat:2308},3],// 3
+    [`Brentford`,`http://oraculo.wederson.com/inglaterra/imagem/brentford.png`,
+        {a:ajuste(10,rodat,66/38,1),d:ajuste(10,rodat,57/38,3),rat:2308},4],// 4
+    [`Brighton`,`http://oraculo.wederson.com/inglaterra/imagem/brighton.png`,
+        {a:ajuste(10,rodat,66/38,1),d:ajuste(10,rodat,59/38,1),rat:2308},5],// 5
+    [`Burnley`,`http://oraculo.wederson.com/inglaterra/imagem/burnley.png`,
+        {a:ajuste(10,rodat,63/46*0.82,0),d:ajuste(10,rodat,36/46*1.20,3),rat:2026},6],// 6
+    [`Chelsea`,`http://oraculo.wederson.com/inglaterra/imagem/chelsea.png`,
+        {a:ajuste(10,rodat,64/38,0),d:ajuste(10,rodat,43/38,0),rat:2308},7],// 7
+    [`Crystal Palace`,`http://oraculo.wederson.com/inglaterra/imagem/crystal.png`,
+        {a:ajuste(10,rodat,51/38,0),d:ajuste(10,rodat,51/38,0),rat:2308},8],// 8
+    [`Everton`,`http://oraculo.wederson.com/inglaterra/imagem/everton.png`,
+        {a:ajuste(10,rodan,42/38,0),d:ajuste(10,rodan,44/38,0),rat:2308},9],// 9
+    [`Fulham`,`http://oraculo.wederson.com/inglaterra/imagem/fulham.png`,
+        {a:ajuste(10,rodat,54/38,1),d:ajuste(10,rodat,54/38,1),rat:2308},10],// 10
+    [`Leeds United`,`http://oraculo.wederson.com/inglaterra/imagem/leeds.png`,
+        {a:ajuste(10,rodan,95/46*0.82,0),d:ajuste(10,rodan,30/46*1.20,0),rat:2026},11],// 11
+    [`Liverpool`,`http://oraculo.wederson.com/inglaterra/imagem/liverpool.png`,
+        {a:ajuste(10,rodat,2.26,4),d:ajuste(10,rodat,1.07,2),rat:2308},12],// 12
+    [`Man City`,`http://oraculo.wederson.com/inglaterra/imagem/mancity.png`,
+        {a:ajuste(10,rodat,72/38,4),d:ajuste(10,rodat,44/38,0),rat:2308},13],// 13
+    [`Man United`,`http://oraculo.wederson.com/inglaterra/imagem/manunited.png`,
+        {a:ajuste(10,rodat,44/38,0),d:ajuste(10,rodat,54/38,1),rat:2308},14],// 14
+    [`Newcastle`,`http://oraculo.wederson.com/inglaterra/imagem/newcastle.png`,
+        {a:ajuste(10,rodat,68/38,0),d:ajuste(10,rodat,47/38,0),rat:2308},15],// 15
+    [`Nottingham`,`http://oraculo.wederson.com/inglaterra/imagem/nottingham.png`,
+        {a:ajuste(10,rodat,58/38,3),d:ajuste(10,rodat,46/38,1),rat:2308},16],// 16
+    [`Sunderland`,`http://oraculo.wederson.com/inglaterra/imagem/sunderland.png`,
+        {a:ajuste(10,rodat,58/46*0.82,3),d:ajuste(10,rodat,44/46*1.20,0),rat:2026},17],// 17
+    [`Tottenham`,`http://oraculo.wederson.com/inglaterra/imagem/tottenham.png`,
+        {a:ajuste(10,rodat,64/38,3),d:ajuste(10,rodat,65/38,0),rat:2308},18],// 18
+    [`West Ham`,`http://oraculo.wederson.com/inglaterra/imagem/westham.png`,
+        {a:ajuste(10,rodat,46/38,0),d:ajuste(10,rodat,62/38,3),rat:2308},19],// 19
+    [`Wolves`,`http://oraculo.wederson.com/inglaterra/imagem/wolves.png`,
+        {a:ajuste(10,rodat,54/38,0),d:ajuste(10,rodat,69/38,4),rat:2308},20]// 20
+]
