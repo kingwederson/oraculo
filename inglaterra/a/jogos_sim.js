dc9c9b980b29e1d0c0c50ff3ed97035a70bbf441
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2025-08-17 15:03:04 -0300

    Add files via upload

---
+++ inglaterra/a/jogos_sim.js
@@ -0,0 +1,29 @@
+var tabelaRRinicial = [[equipe[0][0]]]
+
+function criarRR(){
+    for(let r = 1;r<equipe.length;r++){
+        tabelaRRinicial[0].push(equipe[r][0])
+    }
+    for(let r = 1;r<equipe.length;r++){
+        tabelaRRinicial.push([equipe[r][0]])
+    }
+    for(let r = 1;r<equipe.length;r++){
+        for(let s=1;s<equipe.length;s++){
+            tabelaRRinicial[r].push([,])
+        }
+    }
+}
+/*
+var tabelaRRinicial = [[equipe[0][0]]]
+for(let r = 1;r<=1+equipe.length;r++){
+    tabelaRRinicial[0].push(equipe[r][0])
+}
+for(let r = 1;r<=1+equipe.length;r++){
+    tabelaRRinicial.push(equipe[r][0])
+}
+for(let r = 1;r<=1+equipe.length;r++){
+    for(let s=1;s<=1+equipe.length;s++){
+        tabelaRRinicial[r].push([,])
+    }
+}
+*/
--- inglaterra/a/regras.js
+++ inglaterra/a/regras.js
@@ -1,14 +1,28 @@
 function compara(alfa, omega){
+    var omegacasa = typeof tabelaRR[omega[27]][alfa[27]][0] === "number" ? tabelaRR[omega[27]][alfa[27]][0] : 0;
+    var omegafora = typeof tabelaRR[alfa[27]][omega[27]][1] === "number" ? tabelaRR[alfa[27]][omega[27]][1] : 0;
+    var alfacasa = typeof tabelaRR[alfa[27]][omega[27]][0] === "number" ? tabelaRR[alfa[27]][omega[27]][0] : 0;
+    var alfafora = typeof tabelaRR[omega[27]][alfa[27]][1] === "number" ? tabelaRR[omega[27]][alfa[27]][1] : 0;
+    var confrontodireto = omegacasa + omegafora - alfacasa - alfafora;
+    var golqualificado = omegafora - alfafora;
     if(omega[3]==alfa[3]){
         if(omega[7]==alfa[7]){
             if(omega[8]==alfa[8]){
-                //console.log(JSON.stringify(tabelaRR))
-                //console.log('empatou')
-                return Math.random()-0.5
+                if(confrontodireto == 0){
+                    if(golqualificado == 0){
+                        return Math.random()-0.5
+                    }else{
+                        return golqualificado
+                    }
+                }else{
+                    return confrontodireto
+                }
+            }else{
+                return omega[8]-alfa[8]
             }
-            return omega[8]-alfa[8]
+        }else{
+            return omega[7]-alfa[7]
         }
-        return omega[7]-alfa[7]
     }else{
         return omega[3]-alfa[3]
     }
