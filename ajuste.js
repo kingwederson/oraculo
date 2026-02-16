6d4cdd8f4a7da441c2cacf86debc8e0a17b41a0a
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2026-02-16 14:47:20 -0300

    Add files via upload

--- ajuste.js
+++ ajuste.js
@@ -1,4 +1,12 @@
 function ajuste(range,rodada,passada,atual){
-    let media = (passada*(range-rodada)+atual*rodada)/range
+    let termo1 = passada*(range-rodada)
+    let termo2 = atual*rodada
+    let media = (termo1+termo2)/range
+    return Number(media.toFixed(2))
+}
+function ajuste10(rodada,passada,atual){
+    let termo1 = passada*(10-rodada)
+    let termo2 = atual*rodada
+    let media = (termo1+termo2)/10
     return Number(media.toFixed(2))
 }
