bf39b4f54fde77fde4503da9b9abd6af44a7a85b
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2026-09-13 18:01:17 -0300

    Add files via upload

--- brasil/a/ficheiro.js
+++ brasil/a/ficheiro.js
@@ -76,7 +76,7 @@ var equipe = [
         },12],
     [`Mirassol`,`https://oraculo.wederson.com/brasil/imagem/mirassol.png`,
         {
-            a:12/rodat,
+            a:13/rodat,
             d:18/rodat,
             rat:1914
         },13],
@@ -119,7 +119,7 @@ var equipe = [
     [`Vitória`,`https://oraculo.wederson.com/brasil/imagem/vitoria.png`,
         {
             a:6/rodat,
-            d:13/rodat,
+            d:14/rodat,
             rat:1914
         },20]
 ]
